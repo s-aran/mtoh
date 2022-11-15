573b9ef54418f0b1f5fda189ac30b4cdbe6805c3
s.aran <s-aran@users.noreply.github.com>
2022-11-15 23:37:41 +0900

    move emoji replacement function and add test

--- src/html_gen.rs
+++ src/html_gen.rs
@@ -1,9 +1,8 @@
-use crate::md_event::workarea::{self, Workarea};
+use pulldown_cmark::{Tag, Event};
+
+use crate::md_event::workarea::Workarea;
 use crate::md_event::{end, html, start, text};
 use crate::settings::Settings;
-use linked_hash_set::LinkedHashSet;
-use pulldown_cmark::{CowStr, Event, Tag};
-use std::ops::Range;
 
 pub fn setup_parser<'a, I>(iter: I, settings: &Settings) -> impl Iterator<Item = Event<'a>>
 where
@@ -64,39 +63,3 @@ where
     workarea.events.into_iter()
 }
 
-pub fn replace_emoji_shortcode<'a>(
-    workarea: &Workarea,
-    content: &CowStr,
-) -> Result<String, String> {
-    let r = &workarea.re.emoji_shortcode;
-    let mut result = content.to_owned().to_string();
-    let mut range_set: LinkedHashSet<Range<usize>> = LinkedHashSet::new();
-
-    let mut pos = 0;
-    // find emoji shortcodes (including "maybe")
-    while r.is_match_at(&content, pos) {
-        let c = r.find_at(&content, pos).unwrap();
-        range_set.insert(c.range());
-        pos = c.end() - 2;
-    }
-
-    // replacing the LinkedHashSet with an actual emoji from the emoji shortcodes
-    for v in range_set.iter().rev() {
-        // get emoji shortcode. e.g. :shower:
-        let k = content.to_string().as_str()[v.start..v.end].to_string();
-        // :shower: -> shower
-        let shortcode = &k[1..k.len() - 1];
-        match emojis::get_by_shortcode(shortcode) {
-            Some(e) => {
-                // replace is in reverse order of the emoji shortcodes,
-                // so there is no need to recalculate the index (v.start and v.end)
-                let before = &result[..v.start];
-                let after = &result[v.end..];
-                result = [before, e.as_str(), after].join("");
-            }
-            None => {}
-        }
-    }
-
-    Ok(result.to_string())
-}
--- src/md_event/text.rs
+++ src/md_event/text.rs
@@ -1,6 +1,11 @@
-use super::workarea::Workarea;
-use crate::{html_gen::replace_emoji_shortcode, settings::Settings};
+use std::ops::Range;
+
+use linked_hash_set::LinkedHashSet;
 use pulldown_cmark::{CowStr, Event};
+use regex::Regex;
+
+use super::workarea::Workarea;
+use crate::settings::Settings;
 
 pub fn event<'a>(
     workarea: &mut Workarea<'a>,
@@ -13,13 +18,115 @@ pub fn event<'a>(
         return;
     }
 
-    match replace_emoji_shortcode(workarea, content) {
-        Ok(s) => {
-            workarea.push_event(&Event::Text(s.into()));
-            return;
+    let result = replace_emoji_shortcode(&workarea.re.emoji_shortcode, content);
+    workarea.push_event(&Event::Text(result.into()));
+}
+
+fn replace_emoji_shortcode<'a>(re: &Regex, content: &CowStr) -> String {
+    let mut result = content.to_owned().to_string();
+    let mut range_set: LinkedHashSet<Range<usize>> = LinkedHashSet::new();
+
+    let mut pos = 0;
+    // find emoji shortcodes (including "maybe")
+    while re.is_match_at(&content, pos) {
+        let c = re.find_at(&content, pos).unwrap();
+        range_set.insert(c.range());
+        pos = c.end() - 2;
+    }
+
+    // replacing the LinkedHashSet with an actual emoji from the emoji shortcodes
+    for v in range_set.iter().rev() {
+        // get emoji shortcode. e.g. :shower:
+        let k = content.to_string().as_str()[v.start..v.end].to_string();
+        // :shower: -> shower
+        let shortcode = &k[1..k.len() - 1];
+        match emojis::get_by_shortcode(shortcode) {
+            Some(e) => {
+                // replace is in reverse order of the emoji shortcodes,
+                // so there is no need to recalculate the index (v.start and v.end)
+                let before = &result[..v.start];
+                let after = &result[v.end..];
+                result = [before, e.as_str(), after].join("");
+            }
+            None => {}
         }
-        Err(e) => {
-            panic!("{}", e);
-        }
+    }
+
+    result.to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use pulldown_cmark::CowStr;
+
+    use crate::md_event::workarea::Workarea;
+
+    use super::replace_emoji_shortcode;
+
+    fn replace_emoji(content: &str) -> String {
+        let workarea = Workarea::new();
+        replace_emoji_shortcode(&workarea.re.emoji_shortcode, &CowStr::from(content))
+    }
+
+    #[test]
+    fn test_emoji_only_1() {
+        let content = ":rocket:";
+        assert_eq!(replace_emoji(content), "🚀");
+    }
+
+    #[test]
+    fn test_emoji_only_many_colon() {
+        let content = ": :: :::rocket::: :: :";
+        assert_eq!(replace_emoji(content), ": :: ::🚀:: :: :");
+    }
+
+    #[test]
+    fn test_emoji_multiple() {
+        let content = ":rocket::shower::alien:";
+        assert_eq!(replace_emoji(content), "🚀🚿👽");
+    }
+
+    #[test]
+    fn test_emoji_multiple_with_space() {
+        let content = ":rocket: :shower: :alien:";
+        assert_eq!(replace_emoji(content), "🚀 🚿 👽");
+    }
+
+    #[test]
+    fn test_emoji_with_string() {
+        let content = "rocket: :rocket: shower::shower: alien:alien:";
+        assert_eq!(replace_emoji(content), "rocket: 🚀 shower:🚿 alien👽");
+    }
+
+    #[test]
+    fn test_emoji_with_japanese() {
+        let content = "ロケット: :rocket:　シャワー::shower:\tエイリアン:alien:";
+        assert_eq!(
+            replace_emoji(content),
+            "ロケット: 🚀　シャワー:🚿\tエイリアン👽"
+        );
+
+        // with surrogate
+        let content = "𩸽ロケット: 𩸽:rocket:𩸽　シャワー:𩸽:shower:𩸽\tエイリアン𩸽:alien:𩸽";
+        assert_eq!(
+            replace_emoji(content),
+            "𩸽ロケット: 𩸽🚀𩸽　シャワー:𩸽🚿𩸽\tエイリアン𩸽👽𩸽"
+        );
+    }
+
+    #[test]
+    fn test_emoji_with_unknown_shortcode() {
+        let content = "ロケット: :rocket1:　シャワー::🖤:\tエイリアン:𩸽:";
+        assert_eq!(
+            replace_emoji(content),
+            "ロケット: :rocket1:　シャワー::🖤:\tエイリアン:𩸽:"
+        );
+    }
+
+    #[test]
+    fn test_emoji_without_shortcode() {
+        let content =
+            "::Alice was beginning to get very tired of sitting by her sister on the bank::";
+        assert_eq!(replace_emoji(content), content);
     }
 }
