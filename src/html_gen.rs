23d0f1d81602c009d178d30bf43150e3e5de8b98
s.aran <s-aran@users.noreply.github.com>
2022-11-20 12:25:42 +0900

    add pasing metadata test

--- src/html_gen.rs
+++ src/html_gen.rs
@@ -202,4 +202,122 @@ plain text
         );
         assert_eq!(expected_output, actual_output);
     }
+
+    #[test]
+    fn test_metadata_with_emoji() {
+        let md = r#"# Hello
+<!-- :🚿test🚿: 🚿data🚿 -->
+<!--- :🚿author🚿: 🚿hogehoge piyopiyo🚿 --->
+<!--- 
+    :🚿memo🚿: 🚿テストデータ🚿
+    ::🚿ignored🚿: 🚿test🚿
+--->
+<!--
+    :🚿ignored2🚿: 🚿test🚿
+-->
+<!--
+    :🚿ignored3🚿: 🚿test🚿 -->
+<!---
+    :🚿date🚿: 🚿2022/11/19🚿 --->
+<!---
+    :🚿pat1🚿: 🚿value🚿
+--->
+<!--- :🚿pat2🚿: 🚿value🚿 --->
+<!---
+    :🚿pat3🚿: 🚿value🚿 --->
+<!--- :🚿pat4🚿: 🚿value🚿
+--->
+plain text
+"#;
+
+        let expected_output = r#"<h1>Hello</h1>
+<!-- :🚿test🚿: 🚿data🚿 -->
+<!--
+    :🚿ignored2🚿: 🚿test🚿
+-->
+<!--
+    :🚿ignored3🚿: 🚿test🚿 -->
+<p>plain text</p>
+"#;
+        let expected_metadata: BTreeMap<String, String> = BTreeMap::from([
+            (
+                "🚿author🚿".to_string(),
+                "🚿hogehoge piyopiyo🚿".to_string(),
+            ),
+            ("🚿date🚿".to_string(), "🚿2022/11/19🚿".to_string()),
+            ("🚿memo🚿".to_string(), "🚿テストデータ🚿".to_string()),
+            ("🚿pat1🚿".to_string(), "🚿value🚿".to_string()),
+            ("🚿pat2🚿".to_string(), "🚿value🚿".to_string()),
+            ("🚿pat3🚿".to_string(), "🚿value🚿".to_string()),
+            ("🚿pat4🚿".to_string(), "🚿value🚿".to_string()),
+        ]);
+        let (actual_output, actual_metadata) = generate(md);
+
+        assert_eq!(
+            expected_metadata,
+            actual_metadata
+                .into_iter()
+                .collect::<BTreeMap<String, String>>(),
+        );
+        assert_eq!(expected_output, actual_output);
+    }
+
+    #[test]
+    fn test_metadata_with_surrogate() {
+        let md = r#"# Hello
+<!-- :𩸽test𩸽: 𩸽data𩸽 -->
+<!--- :𩸽author𩸽: 𩸽hogehoge piyopiyo𩸽 --->
+<!--- 
+    :𩸽memo𩸽: 𩸽テストデータ𩸽
+    ::𩸽ignored𩸽: 𩸽test𩸽
+--->
+<!--
+    :𩸽ignored2𩸽: 𩸽test𩸽
+-->
+<!--
+    :𩸽ignored3𩸽: 𩸽test𩸽 -->
+<!---
+    :𩸽date𩸽: 𩸽2022/11/19𩸽 --->
+<!---
+    :𩸽pat1𩸽: 𩸽value𩸽
+--->
+<!--- :𩸽pat2𩸽: 𩸽value𩸽 --->
+<!---
+    :𩸽pat3𩸽: 𩸽value𩸽 --->
+<!--- :𩸽pat4𩸽: 𩸽value𩸽
+--->
+plain text
+"#;
+
+        let expected_output = r#"<h1>Hello</h1>
+<!-- :𩸽test𩸽: 𩸽data𩸽 -->
+<!--
+    :𩸽ignored2𩸽: 𩸽test𩸽
+-->
+<!--
+    :𩸽ignored3𩸽: 𩸽test𩸽 -->
+<p>plain text</p>
+"#;
+        let expected_metadata: BTreeMap<String, String> = BTreeMap::from([
+            (
+                "𩸽author𩸽".to_string(),
+                "𩸽hogehoge piyopiyo𩸽".to_string(),
+            ),
+            ("𩸽date𩸽".to_string(), "𩸽2022/11/19𩸽".to_string()),
+            ("𩸽memo𩸽".to_string(), "𩸽テストデータ𩸽".to_string()),
+            ("𩸽pat1𩸽".to_string(), "𩸽value𩸽".to_string()),
+            ("𩸽pat2𩸽".to_string(), "𩸽value𩸽".to_string()),
+            ("𩸽pat3𩸽".to_string(), "𩸽value𩸽".to_string()),
+            ("𩸽pat4𩸽".to_string(), "𩸽value𩸽".to_string()),
+        ]);
+        let (actual_output, actual_metadata) = generate(md);
+
+        assert_eq!(
+            expected_metadata,
+            actual_metadata
+                .into_iter()
+                .collect::<BTreeMap<String, String>>(),
+        );
+        assert_eq!(expected_output, actual_output);
+    }
 }
